bbe4fd22bcef31c45e5864cf20d50615abb46752
Lizkebe Y Manjuk <lizkebeyakubu@gmail.com>
2023-10-14 18:31:24 -0400

    Update MajorityElement.java
    Added function for te Moore's algorithm to find majority element

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -26,6 +26,32 @@ public class MajorityElement {
      * @param arr
      */
     private static void mooreVoting(int[] arr) {
+        int count = 0, candidate = -1;
+ 
+        // Finding majority candidate
+        for (int index = 0; index < nums.length; index++) {
+          if (count == 0) {
+            candidate = nums[index];
+            count = 1;
+          }
+          else {
+            if (nums[index] == candidate)
+              count++;
+            else
+              count--;
+          }
+        }
+ 
+        // Checking if majority candidate occurs more than
+        // n/2 times
+        count = 0;
+        for (int index = 0; index < nums.length; index++) {
+          if (nums[index] == candidate)
+            count++;
+        }
+        if (count > (nums.length / 2))
+          return candidate;
+        return -1;   
     }
 
     private static int solutionMap(int[] nums) {
